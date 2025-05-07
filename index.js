2586ade245949d26bb567499c6f2a432c011a96d
Ana <stoichitaana07@gmail.com>
2025-05-07 21:41:13 +0300

    Fixed problems in static gallery

--- index.js
+++ index.js
@@ -1,79 +1,108 @@
-const express= require("express");
-const path= require("path");
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
 
+const app = express();
 
-app= express();
-
-v=[10,27,23,44,15]
 app.set('view engine', 'ejs');
 app.use('/Resurse/Imagini', express.static(path.join(__dirname, 'Resurse/Imagini')));
 app.set('views', path.join(__dirname, 'views'));
 
-nrImpar=v.find(function(elem){return elem % 100 == 1})
-console.log(nrImpar)
-
-console.log("Folderul proiectului: ", __dirname)
-console.log("Calea fisierului index.js: ", __filename)
-console.log("Folderul curent de lucru: ", process.cwd())
+console.log("Folderul proiectului: ", __dirname);
+console.log("Calea fisierului index.js: ", __filename);
+console.log("Folderul curent de lucru: ", process.cwd());
 
 app.set("view engine", "ejs");
 
-obGlobal={
-    obErori:null
-}
-app.get(["/","/index","/home"], function(req, res){
-    const imagini = [
-        { fisier: "pandi.jpg", fisier_mediu: "pandi_mediu.jpg", descriere: "Pandi", intervale_zile: [["luni", "miercuri"]] },
-    ];
+const obGlobal = {
+    obErori: null,
+    imagini: null
+};
 
-    res.render("pagini/index", { ip: req.ip, imagini: imagini });
+// Inițializare imagini
+function initImagini() {
+    let continutFisier = fs.readFileSync(path.join(__dirname, "json/galerie.json")).toString("utf-8");
+    let obImagini = JSON.parse(continutFisier);
+    
+    let vectorImagini = obImagini.imagini.map(function(elem) {
+        return {
+            fisier: elem.cale_imagine,
+            fisier_mediu: elem.cale_imagine.replace(".png", "_mediu.png"),
+            descriere: elem.descriere,
+            titlu: elem.titlu,
+            sfert_ora: elem.sfert_ora,
+            // Determinăm intervalele de zile în funcție de sfertul de oră
+            // Pentru demo, vom folosi un algoritm simplu bazat pe sfertul de oră
+            intervale_zile: determinaraZileDupaSfert(elem.sfert_ora)
+        };
+    });
+    
+    obGlobal.imagini = vectorImagini;
+    console.log("Imagini inițializate:", obGlobal.imagini.length);
+}
+
+// Funcție pentru determinarea zilelor de afișare în funcție de sfertul de oră
+function determinaraZileDupaSfert(sfert) {
+    // Convertim la număr
+    const sfertNr = parseInt(sfert);
+    
+    switch(sfertNr) {
+        case 1:
+            return [["luni", "marti"]];
+        case 2:
+            return [["miercuri", "joi"]];
+        case 3:
+            return [["vineri", "sambata"]];
+        case 4:
+            return [["duminica", "duminica"]];
+        default:
+            return [["luni", "duminica"]]; // Implicit toate zilele
+    }
+}
+
+app.get(["/", "/index", "/home"], function(req, res) {
+    res.render("pagini/index", { ip: req.ip, imagini: obGlobal.imagini });
 });
 
-function initErori(){
-    let continut = fs.readFileSync(path.join(__dirname,"./json/erori.json")).toString("utf-8");
-    console.log(continut)
-    obGlobal.obErori=JSON.parse(continut)
-    console.log(obGlobal.obErori)
+function initErori() {
+    let continut = fs.readFileSync(path.join(__dirname, "./json/erori.json")).toString("utf-8");
+    obGlobal.obErori = JSON.parse(continut);
     
-    obGlobal.obErori.eroare_default.imagine=path.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine)
-    for (let eroare of obGlobal.obErori.info_erori){
-        eroare.imagine=path.join(obGlobal.obErori.cale_baza, eroare.imagine)
+    obGlobal.obErori.eroare_default.imagine = path.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine);
+    for (let eroare of obGlobal.obErori.info_erori) {
+        eroare.imagine = path.join(obGlobal.obErori.cale_baza, eroare.imagine);
     }
-    console.log(obGlobal.obErori)
-
 }
 
-initErori()
+initErori();
+initImagini();
 
-function afisareEroare(res, identificator, titlu, text, imagine){
-    let eroare= obGlobal.obErori.info_erori.find(function(elem){ 
-                        return elem.identificator==identificator
-                    });
-    if(eroare){
-        if(eroare.status)
-            res.status(identificator)
-        var titluCustom=titlu || eroare.titlu;
-        var textCustom=text || eroare.text;
-        var imagineCustom=imagine || eroare.imagine;
-
-
+function afisareEroare(res, identificator, titlu, text, imagine) {
+    let eroare = obGlobal.obErori.info_erori.find(function(elem) { 
+        return elem.identificator == identificator;
+    });
+    
+    if (eroare) {
+        if (eroare.status)
+            res.status(identificator);
+        var titluCustom = titlu || eroare.titlu;
+        var textCustom = text || eroare.text;
+        var imagineCustom = imagine || eroare.imagine;
     }
-    else{
-        var err=obGlobal.obErori.eroare_default
-        var titluCustom=titlu || err.titlu;
-        var textCustom=text || err.text;
-        var imagineCustom=imagine || err.imagine;
-
-
+    else {
+        var err = obGlobal.obErori.eroare_default;
+        var titluCustom = titlu || err.titlu;
+        var textCustom = text || err.text;
+        var imagineCustom = imagine || err.imagine;
     }
-    res.render("pagini/eroare", { //transmit obiectul locals
+    
+    res.render("pagini/eroare", {
         titlu: titluCustom,
         text: textCustom,
         imagine: imagineCustom
-})
-
+    });
 }
+
 app.use('/Resurse/Imagini', express.static(path.join(__dirname, 'Resurse/Imagini')));
 
 app.get("/Resurse/*", function(req, res, next) {
@@ -84,85 +113,79 @@ app.get("/Resurse/*", function(req, res, next) {
     }
 });
 
+app.use("/Resurse", express.static(path.join(__dirname, "Resurse")));
 
-app.use("/Resurse", express.static(path.join(__dirname,"Resurse")))
-app.get("/favicon.ico", function(req, res){
-    res.sendFile(path.join(__dirname, "Resurse/Imagini/favicon/favicon.io"))
-})
+app.get("/favicon.ico", function(req, res) {
+    res.sendFile(path.join(__dirname, "Resurse/Imagini/favicon/favicon.ico"));
+});
 
-app.get(["/","/index","/home"], function(req, res){
-    res.render("pagini/index",{ip:req.ip});
-})
-
-app.get("/despre", function(req, res){
+app.get("/despre", function(req, res) {
     res.render("pagini/despre");
-})
+});
 
-app.get("/index/a", function(req, res){
+app.get("/galerie", function(req, res) {
+    res.render("pagini/galerie", { imagini: obGlobal.imagini });
+});
+
+app.get("/index/a", function(req, res) {
     res.render("pagini/index");
-})  
+});
 
+app.get("/cerere", function(req, res) {
+    res.send("<p style='color:blue'>Buna ziua</p>");
+});
 
-app.get("/cerere", function(req, res){
-    res.send("<p style='color:blue'>Buna ziua</p>")
-})
+app.get("/fisier", function(req, res, next) {
+    res.sendFile(path.join(__dirname, "package.json"));
+});
 
+app.get("/abc", function(req, res, next) {
+    res.write("Data curenta: ");
+    next();
+});
 
-app.get("/fisier", function(req, res, next){
-    res.sendfile(path.join(__dirname,"package.json"));
-})
+app.get("/abc", function(req, res, next) {
+    res.write((new Date())+"");
+    res.end();
+    next();
+});
 
+app.get("/abc", function(req, res, next) {
+    console.log("------------");
+});
 
-app.get("/abc", function(req, res, next){
-    res.write("Data curenta: ")
-    next()
-})
-
-app.get("/abc", function(req, res, next){
-    res.write((new Date())+"")
-    res.end()
-    next()
-})
-
-
-app.get("/abc", function(req, res, next){
-    console.log("------------")
-})
-
-app.get("/*ejs", function(req, res, next){
+app.get("/*ejs", function(req, res, next) {
     afisareEroare(res, 400);
 });
 
-
-app.get("/*", function(req, res, next){
-    try{
-        res.render("pagini" + req.url,function (err, rezultatRandare){
-            if (err){
-                if(err.message.startsWith("Failed to lookup view")){
-                    afisareEroare(res,404);
+app.get("/*", function(req, res, next) {
+    try {
+        res.render("pagini" + req.url, function(err, rezultatRandare) {
+            if (err) {
+                if (err.message.startsWith("Failed to lookup view")) {
+                    afisareEroare(res, 404);
                 }
-                else{
+                else {
                     afisareEroare(res);
                 }
             }
-            else{
+            else {
                 console.log(rezultatRandare);
-                res.send(rezultatRandare)
+                res.send(rezultatRandare);
             }
         });
     }
-    catch(errRandare){
-        if(errRandare.message.startsWith("Cannot find module")){
-            afisareEroare(res,404);
+    catch (errRandare) {
+        if (errRandare.message.startsWith("Cannot find module")) {
+            afisareEroare(res, 404);
         }
-        else{
+        else {
             afisareEroare(res);
         }
     }
-})
+});
 
-
-const vect_foldere = ["temp"];
+const vect_foldere = ["temp", "json"];
 for (let folder of vect_foldere) {
     let folderPath = path.join(__dirname, folder);
     if (!fs.existsSync(folderPath)) {
@@ -171,6 +194,5 @@ for (let folder of vect_foldere) {
     }
 }
 
-
 app.listen(8080);
-console.log("Serverul a pornit")
+console.log("Serverul a pornit");
